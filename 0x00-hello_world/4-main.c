b99ab9900c38c3d1bec2b1cbe5cff3dc270b831e
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-01 14:21:12 -0700

    new commit

--- 0x00-hello_world/4-main.c
+++ 0x00-hello_world/4-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void){
-	
-	char myStr[] = "\"Programming is like building a multilingual puzzle";
-	puts(myStr);
+
+	puts("\"Programming is like building a multilingual puzzle");
 
   
   return(0);
