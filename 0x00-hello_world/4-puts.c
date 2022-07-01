ffd51d99b3e578a70a30d69397346daa58b822b5
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-01 14:45:54 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -9,6 +9,5 @@ int main(void)
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-  
 	return(0);
 }
