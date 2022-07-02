8d6b9ca11e6bd91f5d9ea107041dd12b3a8d0be2
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-02 04:56:57 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *This is just a description
+ *The main function is the entry point of the program
+ *It returns 0 if successful
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf(%S,"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
