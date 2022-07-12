79bebad08036c1809ac19d7afeb89a8f57af49af
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-12 04:50:07 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+/*
+ * swap_int - swaps two integers value
+ * @a: pointer that points to integer
+ * @b: pointer that points to integer
+ */
+void swap_int(int *a,int *b)
+{
+	*a = *b;
+	*b = *a;
+}
