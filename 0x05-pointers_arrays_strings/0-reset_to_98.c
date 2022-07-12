bb908f0fb1cef4e9c27b9f33df0043eba8ec7a57
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-12 02:21:53 -0700

    new commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
  * reset_to_98 - resets a  pointer value to 98
- * n - parameter that holds the value to point to
+ * @n: parameter that holds the value to point to
  */
 void reset_to_98(int *n)
 {
