aa05c7bca1fb8dfb1fd06aac6f52203b0085bf1a
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-12 09:57:43 -0700

    new commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * puts - displays a string to stdout
  * @str: pointer to string
@@ -8,13 +9,8 @@ int _puts(char *str)
 	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{}
-	j = i;
-	char txt[j];
-	for (i = 0; str[i] != '\0'; i++)
 	{
-		txt[i] = str[i];
-		print("%s",txt[i]);
+		print("%s",str[i]);
 	}
 	print("\n");
 }
