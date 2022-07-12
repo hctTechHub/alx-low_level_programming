d9ca4389374b32704d7880a70b835e5ed62491fb
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-12 09:10:07 -0700

    new commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 /**
  * _strlen - returns length of string
  * @s: character that points to string
+ * Return: returns the value of i
  */
 int _strlen(char *s)
 {
 	int i;
-	for(i = 0; s[i] != '\0'; i++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 
 	}
