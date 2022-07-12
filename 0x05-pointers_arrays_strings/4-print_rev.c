71e3c49b931411f0898bc9204a6e318d7088b829
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-12 10:28:34 -0700

    new commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
 void print_rev(char *s)
 {
 	int i;
-	
-	for (i=0; s[i] != '\0'; i++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{}
 	while (i >= 0)
 	{
-		printf("%c", s[i-1]);
+		printf("%c", s[i - 1]);
 		i--;
 	}
 	printf("\n");
