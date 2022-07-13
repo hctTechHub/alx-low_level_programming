c432726ec5bae9e17e817020aa9eded9eecd5071
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-13 12:30:15 -0700

    new commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,12 +4,12 @@
  * _strcpy - copy string
  * @dest: destination string
  * @src: source string
- * @Return: pointer
+ * Return: pointer
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	for (i = 0; i <= _strlen(src); i++)
 	{
 		dest[i] = src[i];
