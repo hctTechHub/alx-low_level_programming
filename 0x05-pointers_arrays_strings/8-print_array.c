71607bcae5382e3a12b34be52430c8eb471893e6
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-13 11:39:21 -0700

    new commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,6 +10,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%i, ", a[i]);
+		if (i ! = n - 1)
+		{
+			printf("%i, ", a[i]);
+		}
+		else
+		{
+			printf("%i", a[i]);
+		}
 	}
 }
