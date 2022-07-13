20048cafadaaece6a0a0ba2779cea1a458471a6a
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-13 12:40:35 -0700

    new commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include "2-strlen.c"
 /**
  * rev_string - prints a string in reverse order
  * @s: string to reverse
  */
 void rev_string(char *s)
 {
-	int i,len;
-	
-	len = sizeof(s)/sizeof(char);
-	for ( i  = 0; i < len; i++)
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
 	{
-		printf("%c", s[i]);
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
 	}
-	printf("\n");
 }
