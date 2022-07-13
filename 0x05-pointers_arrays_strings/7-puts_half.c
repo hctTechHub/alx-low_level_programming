fae2d41e9dd2c30348efe01774c8a7506da7ce11
hctTechHub <engrtemidayo2017@gmail.com>
2022-07-13 02:51:58 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/***
+ * puts_half - prints half string
+ * @str - string to print
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int len = _stren(str);
+	int halfLen = len / 2;
+	int n = (len - 1) / 2;
+	if(len % 2 == 0)
+	{
+		for(i = halfLen; i < len; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	else
+	{
+		for(i = n+2; i < len; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+      	printf("\n");
+}	
